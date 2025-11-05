b3d8ade3289e97749bce14478d56e5e450656b0d
shayam73 <shayanm123654@gmail.com>
2025-11-05 17:12:19 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // server.js
-import("./server/index.js")
-  .then(({ default: startServer }) => {
-    startServer();
-  })
-  .catch((err) => {
-    console.error("❌ Error starting The Lounge server:", err);
-    process.exit(1);
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import chalk from "chalk";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("✅ The Lounge custom server is running successfully!");
+});
+
+io.on("connection", (socket) => {
+  console.log(chalk.green("🟢 A user connected"));
+  socket.on("disconnect", () => {
+    console.log(chalk.yellow("🔴 A user disconnected"));
   });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(chalk.blue(`🚀 Server running on port ${PORT}`));
+});
