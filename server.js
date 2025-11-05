1d88a8dc0e5d815c59e3012530603436f7bc5b57
shayam73 <shayanm123654@gmail.com>
2025-11-05 16:10:19 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,7 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: { origin: "*" }
-});
-
-app.get("/", (req, res) => {
-  res.send("🚀 The Lounge Web Service is running successfully!");
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+// server.js
+import("./server").then(({ default: startServer }) => {
+  startServer();
+}).catch((err) => {
+  console.error("Error starting The Lounge server:", err);
+  process.exit(1);
 });
